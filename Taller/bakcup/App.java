7af9bfca456ed12b28679e99d7f569316e819a64
agustinfacu <168496366+agustinfacu@users.noreply.github.com>
2024-12-09 20:02:01 -0300

    cuarto commit borrado de bakup

--- Taller/bakcup/App.java
+++
@@ -1,8 +0,0 @@
-import main.controlador.GarajeController;
-
-public class App {
-    public static void main(String[] args) {
-        GarajeController controller = new GarajeController();
-        controller.iniciar();
-    }
-}
